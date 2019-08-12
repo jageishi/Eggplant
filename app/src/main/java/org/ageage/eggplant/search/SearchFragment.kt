32e869d949c329fa048d5a7da74c3d53b732e0f5
JunAgeishi <junageishi@gmail.com>
2019-08-12 11:15:55 +0900

    Add fragments for feed, search and settings

---
+++ app/src/main/java/org/ageage/eggplant/search/SearchFragment.kt
@@ -0,0 +1,29 @@
+package org.ageage.eggplant.search
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import org.ageage.eggplant.R
+
+class SearchFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_search, container, false)
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = SearchFragment()
+    }
+}
---
+++ app/src/main/java/org/ageage/eggplant/settings/SettingsFragment.kt
@@ -0,0 +1,29 @@
+package org.ageage.eggplant.settings
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import org.ageage.eggplant.R
+
+class SettingsFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_settings, container, false)
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = SettingsFragment()
+    }
+}
