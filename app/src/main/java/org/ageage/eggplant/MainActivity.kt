5d924918fadd67de31375c55568a28fd38657b3b
JunAgeishi <junageishi@gmail.com>
2019-08-14 08:41:02 +0900

    Add bottom navigation with BottomNavigationDialogFragment

--- app/src/main/java/org/ageage/eggplant/MainActivity.kt
+++ app/src/main/java/org/ageage/eggplant/MainActivity.kt
@@ -2,6 +2,7 @@ package org.ageage.eggplant
 
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -16,5 +17,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.bottom_app_bar, menu)
         return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            android.R.id.home -> {
+                val fragment = BottomSheetFragment.newInstance()
+                fragment.show(supportFragmentManager, "a")
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
