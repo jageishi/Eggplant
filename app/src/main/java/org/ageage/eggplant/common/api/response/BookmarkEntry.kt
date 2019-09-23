db5af4149097f8445ffcf56708136edea1e4a000
JunAgeishi <junageishi@gmail.com>
2019-09-23 23:22:09 +0900

    Fix bookmarks response name

--- app/src/main/java/org/ageage/eggplant/common/api/response/BookmarkEntry.kt
+++ app/src/main/java/org/ageage/eggplant/common/api/response/BookmarkEntry.kt
@@ -1,10 +1,12 @@
 package org.ageage.eggplant.common.api.response
 
+import com.google.gson.annotations.SerializedName
+
 data class BookmarkEntry(
     val title: String,
     val count: Int,
     val entryUrl: String,
     val screenshot: String,
     val eid: String,
-    val bookmarkResponses: List<BookmarkResponse>
+    @SerializedName("bookmarks") val bookmarkResponses: List<BookmarkResponse>
 )
