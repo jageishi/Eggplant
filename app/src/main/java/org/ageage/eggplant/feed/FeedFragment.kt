483d543429512a8f859c90004299b9ef236b53ac
Jun Ageishi <junageishi@gmail.com>
2019-09-29 23:50:53 +0900

    Change to new design (#43)

--- app/src/main/java/org/ageage/eggplant/feed/FeedFragment.kt
+++ app/src/main/java/org/ageage/eggplant/feed/FeedFragment.kt
@@ -1,6 +1,5 @@
 package org.ageage.eggplant.feed
 
-
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,41 +7,45 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.fragment_feed.*
 import org.ageage.eggplant.R
+import org.ageage.eggplant.common.enums.Category
 import org.ageage.eggplant.common.enums.Mode
-import timber.log.Timber
+
+private const val MODE = "mode"
 
 class FeedFragment : Fragment() {
 
+    private lateinit var mode: Mode
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        Timber.d("${this} : onCreate")
         super.onCreate(savedInstanceState)
+        arguments?.let {
+            mode = it.getSerializable(MODE) as Mode
+        }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        Timber.d("onCreateView")
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_feed, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        Timber.d("onViewCreated")
         super.onViewCreated(view, savedInstanceState)
-        val adapter = FeedModePagerAdapter(childFragmentManager, context)
-        Mode.values().forEach { adapter.addContent(it) }
-        modeViewPager.adapter = adapter
-        modeTab.setupWithViewPager(modeViewPager)
-    }
-
-    override fun onDestroy() {
-        Timber.d("${this} : onDestroy")
-        super.onDestroy()
+        val adapter = FeedCategoryPagerAdapter(childFragmentManager, context)
+        Category.values().forEach { adapter.addContent(mode, it) }
+        categoryViewPager.adapter = adapter
+        categoryTab.setupWithViewPager(categoryViewPager)
     }
 
     companion object {
         @JvmStatic
-        fun newInstance() = FeedFragment()
+        fun newInstance(mode: Mode) =
+            FeedFragment().also { f ->
+                f.arguments = Bundle().also { b ->
+                    b.putSerializable(MODE, mode)
+                }
+            }
     }
 }
