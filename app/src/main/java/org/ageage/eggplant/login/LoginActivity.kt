ff49f7299e7f3694f0c7caaa11ba797fa9beee89
JunAgeishi <junageishi@gmail.com>
2019-11-07 07:13:42 +0900

    Add LoginActivity and  OAuth-related files and settings

---
+++ app/src/main/java/org/ageage/eggplant/login/LoginActivity.kt
@@ -0,0 +1,13 @@
+package org.ageage.eggplant.login
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import org.ageage.eggplant.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
