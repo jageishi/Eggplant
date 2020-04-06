10b6315eb0d1102ce3de5be6a58f4eb70dfe7a52
Jun Ageishi <j.ageishi@r-n-i.jp>
2020-04-06 09:49:09 +0900

    WIP

--- app/src/main/java/org/ageage/eggplant/common/repository/BookmarkRepository.kt
+++ app/src/main/java/org/ageage/eggplant/common/repository/BookmarkRepository.kt
@@ -2,9 +2,7 @@ package org.ageage.eggplant.common.repository
 
 import android.text.format.DateFormat
 import io.reactivex.Observable
-import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.rxkotlin.toObservable
-import io.reactivex.schedulers.Schedulers
 import org.ageage.eggplant.common.api.BookmarkService
 import org.ageage.eggplant.common.api.Client
 import org.ageage.eggplant.common.api.response.mapper.toBookmarks
@@ -15,20 +13,18 @@ import java.util.*
 
 class BookmarkRepository {
 
-    fun fetchBookmarks(url: String): Observable<List<Bookmark>> {
+    suspend fun fetchBookmarks(url: String): Observable<List<Bookmark>> {
         val service =
             Client.retrofitClient(Endpoint.HATENA_BOOKMARK)
                 .create(BookmarkService::class.java)
 
         return service.bookmarkEntry(url)
-            .flatMap { bookmarkEntry ->
-                bookmarkEntry.bookmarkResponses.toObservable()
-                    .subscribeOn(Schedulers.io())
-                    .observeOn(Schedulers.io())
+            .let { bookmarkEntry ->
+                bookmarkEntry.bookmarkResponses
                     .filter {
                         it.comment.isNotEmpty()
                     }
-                    .concatMapEager { bookmark ->
+                    .map { bookmark ->
                         val timestamp =
                             DateFormat.format(
                                 "yyyyMMdd",
@@ -40,10 +36,7 @@ class BookmarkRepository {
                         Client.retrofitClient(Endpoint.HATENA_STAR)
                             .create(BookmarkService::class.java)
                             .startCount("${Endpoint.HATENA_BOOKMARK.url}/${bookmark.user}/${timestamp}#bookmark-${bookmarkEntry.eid}")
-                            .subscribeOn(Schedulers.io())
-                            .observeOn(AndroidSchedulers.mainThread())
                     }
-                    .toList()
                     .map { responses ->
                         bookmarkEntry.bookmarkResponses
                             .filter {
