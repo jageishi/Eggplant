52b42717ae47296243505c8a12e100596409dc2a
JunAgeishi <junageishi@gmail.com>
2021-08-18 09:47:08 +0900

    変更

--- test.rb
+++ test.rb
@@ -1,18 +1,45 @@
 module Danger
-  class CheckTest < Plugin
+  class ImageResourceChecker < Plugin
+
+    attr_accessor :target_densities, :target_extensions
+
+    def target_densities
+      return @target_densities || ["mdpi", "hdpi", "xhdpi", "xxhdpi", "xxxhdpi"]
+    end
+
+    def target_extensions
+      return @target_extensions || ["png", "jpg"]
+    end
+
     def check
-      target_files = git.added_files + git.modified_files + git.deleted_files + git.renamed_files.map { |f| f[:after] }
-      message = ""
-      target_files.each do |file|
-        message << "|`#{file}`|\n"
+      renamed_files = git.renamed_files
+      target_files = git.added_files \
+                        + git.modified_files \
+                        + renamed_files.map { |f| f[:after] } \
+                        - git.deleted_files \
+                        - renamed_files.map { |f| f[:before] }
+
+      regex = /drawable-(#{target_densities.join("|") })\/.*\.(#{target_extensions.join("|")})$/
+      target_image_files = target_files.filter { |f| regex.match?(f) }
+
+      checked_files = []
+      missing_density_files = []
+
+      target_image_files.each do |f|
+        next if checked_files.include?(f)
+        target_densities.each do |d|
+          image_file = f.gsub(/drawable-(#{target_densities.join("|")})/, "drawable-#{d}")
+          missing_density_files << image_file unless File.exists?(image_file)
+          checked_files << image_file
+        end
       end
 
-      if !message.empty?
-        header = "#### 追加、変更、削除されたファイル\n"
-        header << "|File|\n"
+      unless missing_density_files.empty?
+        header = "### 画像ファイルを追加してください:pray:\n"
+        header << "| ファイル |\n"
         header << "| --- |\n"
-        message = header + message
-        markdown(message)
+        message = missing_density_files.map { |f| "| `#{f}` |\n" }.join
+        markdown(header + message)
       end
     end
   end
