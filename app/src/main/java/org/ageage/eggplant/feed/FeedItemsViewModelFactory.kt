8489b21f7e1b0a932f86cba6cb74cb69b2cf68eb
JunAgeishi <junageishi@gmail.com>
2022-10-26 23:38:44 +0900

    Android Studio Dolphinでビルドできるようにした

--- app/src/main/java/org/ageage/eggplant/feed/FeedItemsViewModelFactory.kt
+++ app/src/main/java/org/ageage/eggplant/feed/FeedItemsViewModelFactory.kt
@@ -2,12 +2,13 @@ package org.ageage.eggplant.feed
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
 import org.ageage.eggplant.common.repository.FeedRepository
 
 class FeedItemsViewModelFactory : ViewModelProvider.Factory {
 
     @Suppress("UNCHECKED_CAST")
-    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+    override fun <T : ViewModel> create(modelClass: Class<T>, extras: CreationExtras): T {
         if (modelClass != FeedItemsViewModel::class.java) {
             throw IllegalArgumentException("Illegal ViewModel class.")
         }
